197765a50dd2b079e957709ab1bc5103b8793f5c
Brian Ellis <brian@briansresume.com>
2021-09-26 13:00:12 -0400

    beggining local persistence of data in classes

--- src/Classes/BrowserHistory.ts
+++ src/Classes/BrowserHistory.ts
@@ -9,20 +9,21 @@ export default class BrowserHistory {
     window.history.replaceState({ init_class: init_class } , '', '' );
 
     window.onpopstate = event => {
-      this.render( this[ 'class_list' ][ event.state.init_class ][ 'init' ] );
+      this.render( this[ 'class_list' ][ event.state.init_class ] );
     };
 
-    this.render( this[ 'class_list' ][ init_class ][ 'init' ] );
+    this.render( this[ 'class_list' ][ init_class ] );
   }
 
   forward( init_class: string ): void {
     init_class = init_class.toLowerCase();
     window.history.pushState( { init_class: init_class } , '', '' );
-    this.render( this[ 'class_list' ][ init_class ][ 'init' ] );
+    this.render( this[ 'class_list' ][ init_class ] );
   }
 
-  render( class_init: any ): void {
+  render( acting_class: any ): void {
     this.app_ele_target.innerHTML = '';
-    class_init( this.app_ele_target );
+    console.log( acting_class );
+    acting_class.init( this.app_ele_target , acting_class );
   }
 }
--- src/Pages/PageMySql.ts
+++ src/Pages/PageMySql.ts
@@ -1,9 +1,16 @@
 import CalculatorMySql from '@class/CalculatorMySql';
 
 export default class PageMySql {
-  init( append_element: HTMLElement ): void {
-    const CALC_MYSQL = new CalculatorMySql(),
-      TOTAL_BYTE_ELE = Object.assign(document.createElement('p'), {id: 'total-byte-conversion'}),
+  private dom_info: string;
+  private ref_class;
+
+  constructor() {
+    this.ref_class = new CalculatorMySql();
+    this.dom_info  = '';
+  }
+
+  init( append_element: HTMLElement , acting_class: any ): void {
+    const TOTAL_BYTE_ELE = Object.assign(document.createElement('p'), {id: 'total-byte-conversion'}),
       EDITOR = Object.assign(document.createElement('textarea'), {id: 'mysql-editor'});
 
     EDITOR.style.height       = '70vh';
@@ -14,75 +21,83 @@ export default class PageMySql {
     EDITOR.style.float        = 'left';
     EDITOR.style.marginRight  = '3vh';
 
+    EDITOR.innerHTML = acting_class.dom_info;
+
     EDITOR.addEventListener('keyup', event => {
-      const TARGET = event.target as HTMLTextAreaElement,
-        TEXT = TARGET.value.split('\n');
-
-      let total_size: number = 0,
-        brace_check: string = '',
-        db_data: any = {},
-        db_table: string = '',
-        db_row_ct: string = '',
-        db_table_size: number = 0;
-
-      for (const ROW_VAL of TEXT) {
-        const LINE_VAL = ROW_VAL.replace(/\s+/, '');
-
-        if (LINE_VAL.indexOf('{') > -1) {
-          if ('{' === brace_check) {
-            console.log('Brace Mismatch 2 {');
-            break;
-          }
-
-          db_table_size = 0;
-          brace_check = '{';
-
-          const TABLE_ROW = LINE_VAL.replace('{', '').split(':');
-          db_table = TABLE_ROW[0];
-          db_row_ct = TABLE_ROW[1];
-
-          if (db_table) {
-            if (db_data[db_table]) {
-              console.log('Duplicate table declaration');
-              break;
-            }
-
-            db_data[db_table] = {
-              row_count: parseInt(db_row_ct) > 0 ? parseInt(db_row_ct) : 1,
-              byte_size: 0
-            };
-          }
-        } else if (LINE_VAL.indexOf('}') > -1) {
-          if ('{' !== brace_check) {
-            console.log('Brace Mismatch due to prev value ' + brace_check);
-            break;
-          }
-
-          db_data[db_table]['byte_size'] = db_table_size;
-          brace_check = '}';
-        } else if ( LINE_VAL.trim() ) {
-          const DATA_TYPE = LINE_VAL.split(':')[1];
-          if (DATA_TYPE) {
-            const DATA_VAL = CALC_MYSQL.validType(DATA_TYPE);
-            if (DATA_VAL > 0) db_table_size += DATA_VAL;
-          }
-        }
-      }
-
-      let table_sizes = '';
-      for (const TABLE_NAME in db_data) {
-        const TABLE_TOTAL_SIZE = db_data[TABLE_NAME]['byte_size'] * db_data[TABLE_NAME]['row_count'];
-        total_size += TABLE_TOTAL_SIZE;
-        table_sizes += `
-              ${TABLE_NAME} 
-              \tRows: ${db_data[TABLE_NAME]['row_count']} 
-              \tSize: ${CALC_MYSQL.formatBytes(TABLE_TOTAL_SIZE)}`;
-      }
-
-      const TOTAL_BYTE_ELE = document.getElementById('total-byte-conversion') as HTMLTextAreaElement;
-      TOTAL_BYTE_ELE.innerText = 'Total Database: ' + CALC_MYSQL.formatBytes(total_size) + table_sizes;
+      const TARGET = event.target as HTMLTextAreaElement;
+      acting_class.evalTextArea( TARGET.value , acting_class );
     });
 
     append_element.append(EDITOR, TOTAL_BYTE_ELE);
+    this.evalTextArea( acting_class.dom_info , acting_class );
+  }
+
+  evalTextArea( target_text: string , acting_class: any ) {
+    if ( acting_class ) acting_class.dom_info = target_text;
+    const TEXT = target_text.split('\n');
+
+    let total_size: number = 0,
+      brace_check: string = '',
+      db_data: any = {},
+      db_table: string = '',
+      db_row_ct: string = '',
+      db_table_size: number = 0;
+
+    for ( const ROW_VAL of TEXT ) {
+      const LINE_VAL = ROW_VAL.replace(/\s+/, '');
+
+      if (LINE_VAL.indexOf('{') > -1) {
+        if ('{' === brace_check) {
+          console.log('Brace Mismatch 2 {');
+          break;
+        }
+
+        db_table_size = 0;
+        brace_check = '{';
+
+        const TABLE_ROW = LINE_VAL.replace('{', '').split(':');
+        db_table = TABLE_ROW[0];
+        db_row_ct = TABLE_ROW[1];
+
+        if (db_table) {
+          if (db_data[db_table]) {
+            console.log('Duplicate table declaration');
+            break;
+          }
+
+          db_data[db_table] = {
+            row_count: parseInt(db_row_ct) > 0 ? parseInt(db_row_ct) : 1,
+            byte_size: 0
+          };
+        }
+      } else if (LINE_VAL.indexOf('}') > -1) {
+        if ('{' !== brace_check) {
+          console.log('Brace Mismatch due to prev value ' + brace_check);
+          break;
+        }
+
+        db_data[db_table]['byte_size'] = db_table_size;
+        brace_check = '}';
+      } else if ( LINE_VAL.trim() ) {
+        const DATA_TYPE = LINE_VAL.split(':')[1];
+        if (DATA_TYPE) {
+          const DATA_VAL = this.ref_class.validType(DATA_TYPE);
+          if (DATA_VAL > 0) db_table_size += DATA_VAL;
+        }
+      }
+    }
+
+    let table_sizes = '';
+    for (const TABLE_NAME in db_data) {
+      const TABLE_TOTAL_SIZE = db_data[TABLE_NAME]['byte_size'] * db_data[TABLE_NAME]['row_count'];
+      total_size += TABLE_TOTAL_SIZE;
+      table_sizes += `
+              ${TABLE_NAME} 
+              \tRows: ${db_data[TABLE_NAME]['row_count']} 
+              \tSize: ${this.ref_class.formatBytes(TABLE_TOTAL_SIZE)}`;
+    }
+
+    const TOTAL_BYTE_ELE = document.getElementById('total-byte-conversion') as HTMLTextAreaElement;
+    TOTAL_BYTE_ELE.innerText = 'Total Database: ' + this.ref_class.formatBytes(total_size) + table_sizes;
   }
 }
